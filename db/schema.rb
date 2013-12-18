c385749f3a183e47e71657a35509f9893664ddbd
Joe Sutton <hungryed6@gmail.com>
2013-12-18 15:03:27 -0500

    removed favorites column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131218191413) do
+ActiveRecord::Schema.define(version: 20131218200121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,11 +22,10 @@ ActiveRecord::Schema.define(version: 20131218191413) do
   end
 
   create_table "books", force: true do |t|
-    t.string   "title",                      null: false
+    t.string   "title",      null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "author_id"
-    t.boolean  "favorite",   default: false
     t.integer  "rating"
   end
 
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -4,8 +4,4 @@ describe Book do
   it { should validate_presence_of :title }
   it { should validate_numericality_of :rating}
 
-  it "should default favorite book to false" do
-    book = FactoryGirl.create(:book)
-    expect(book.favorite).to be_false
-  end
 end
